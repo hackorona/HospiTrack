6ab26694b956a9b0d86d5a1215ecf56dcbf52bbc
Shachar Buda <shacharbuda27@gmail.com>
2020-03-23 21:11:17 +0200

    repeat

--- client/App/Sagas/WifiSaga.js
+++ client/App/Sagas/WifiSaga.js
@@ -1,18 +1,26 @@
-import { put, call } from 'redux-saga/effects'
+import { put, call, delay } from 'redux-saga/effects'
 import WifiActions from '../Stores/Wifi/Actions'
 import { wifiService } from '../Services/WifiService'
 
-export function* fetchWifiList() {
-  // Dispatch a redux action using `put()`
-  yield put(WifiActions.fetchWifiListLoading())
+const DELAY = 1000;
+const REPEAT = 10; 
 
-  // Fetch user informations from an API
-  const wifiArr = yield call(wifiService.fetchWifiList)
-  if (wifiArr) {
-    yield put(WifiActions.fetchWifiListSuccess(wifiArr))
-  } else {
-    yield put(
-      WifiActions.fetchWifiListFailure('There was an error while fetching Wifi list.')
-    )
+export function* fetchWifiList() {
+  let counter = REPEAT;
+  while (counter--) { 
+    // Dispatch a redux action using `put()`
+    yield put(WifiActions.fetchWifiListLoading())
+
+    // Fetch user informations from an API
+    const wifiArr = yield call(wifiService.fetchWifiList)
+    if (wifiArr) {
+      yield put(WifiActions.fetchWifiListSuccess(wifiArr))
+    } else {
+      yield put(
+        WifiActions.fetchWifiListFailure('There was an error while fetching Wifi list.')
+      );
+    }
+
+    yield delay(DELAY);
   }
 }
