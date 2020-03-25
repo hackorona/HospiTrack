e3641db0aeb8faa470ce12c812002e1a3bf2b2f2
Shachar Buda <shacharbuda27@gmail.com>
2020-03-25 12:10:04 +0200

    strip screen to text only

--- client/App/Containers/NoPermissionsScreen/NoPermissionsScreen.js
+++ client/App/Containers/NoPermissionsScreen/NoPermissionsScreen.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { TouchableOpacity, Platform, Text, View, Button, ActivityIndicator, Image } from 'react-native'
-import { connect } from 'react-redux'
-import { PropTypes } from 'prop-types'
 import WifiActions from 'App/Stores/Wifi/Actions'
 import { Helpers, Metrics } from 'App/Theme'
 
@@ -31,30 +29,6 @@ class NoPermissionsScreen extends React.Component {
 }
 
 NoPermissionsScreen.propTypes = {
-  user: PropTypes.object,
-  userIsLoading: PropTypes.bool,
-  userErrorMessage: PropTypes.string,
-  fetchUser: PropTypes.func,
-  fetchWifiList: PropTypes.func,
-  liveInEurope: PropTypes.bool,
 }
 
-const mapStateToProps = (state) => ({
-  user: state.example.user,
-  userIsLoading: state.example.userIsLoading,
-  userErrorMessage: state.example.userErrorMessage,
-  liveInEurope: liveInEurope(state),
-  wifiList: state.wifi.wifiList,
-  wifiIsLoading: state.wifi.wifiListIsLoading,
-  wifiErrorMessage: state.wifi.wifiListErrorMessage,
-})
-
-const mapDispatchToProps = (dispatch) => ({
-  fetchUser: () => dispatch(ExampleActions.fetchUser()),
-  fetchWifiList: () => dispatch(WifiActions.fetchWifiList())
-})
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(NoPermissionsScreen)
+export default NoPermissionsScreen;
