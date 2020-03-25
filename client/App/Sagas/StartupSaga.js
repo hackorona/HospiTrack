b4b1d78e9b0bacd77dc30ff1b0328528f850203d
Shachar Buda <shacharbuda27@gmail.com>
2020-03-25 12:03:07 +0200

    ask permissions on startup

--- client/App/Sagas/StartupSaga.js
+++ client/App/Sagas/StartupSaga.js
@@ -1,6 +1,7 @@
-import { put } from 'redux-saga/effects'
+import { put, call } from 'redux-saga/effects'
 import ExampleActions from 'App/Stores/Example/Actions'
 import NavigationService from 'App/Services/NavigationService'
+import { wifiService } from '../Services/WifiService';
 
 /**
  * The startup saga is the place to define behavior to execute when the application starts.
@@ -12,6 +13,15 @@ export function* startup() {
 
   // Add more operations you need to do at startup here
   // ...
+
+  // We check for location permissions granted.
+  const isPermissionsGranted = yield call(wifiService.askPermissions);
+  
+  // w/o location permissions, app is useless. go to NoPermissionsScreen
+  // and beg user to grant permissions.
+  if (!isPermissionsGranted) {
+    return NavigationService.navigateAndReset('NoPermissionsScreen')
+  }
 
   // When those operations are finished we redirect to the main screen
   NavigationService.navigateAndReset('MainScreen')
