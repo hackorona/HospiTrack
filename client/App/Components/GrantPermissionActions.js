f33051d1898a976b387ea2d8132c110477e6375a
Shachar Buda <shacharbuda27@gmail.com>
2020-03-25 14:55:04 +0200

    act on return from settings!

--- client/App/Components/GrantPermissionActions.js
+++ client/App/Components/GrantPermissionActions.js
@@ -2,10 +2,41 @@ import React from 'react'
 import { View } from 'react-native'
 import { Helpers } from '../Theme';
 import FullWidthButton from './FullWidthButton';
+import { AppState, Text } from 'react-native';
 
 export default class GrantPermissionActions extends React.Component {
+  state = {
+    appState: AppState.currentState,
+  };
+
+  componentDidMount() {
+    AppState.addEventListener('change', this._handleAppStateChange);
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this._handleAppStateChange);
+  }
+
+  _handleAppStateChange = (nextAppState) => {
+    if (
+      this.state.appState.match(/inactive|background/) &&
+      nextAppState === 'active'
+    ) {
+      const { isBlocked, requestPermission } = this.props;
+
+      // App has returned to the foreground!
+      if (isBlocked) {
+        // This case means user returned from settings.
+        // Request permission to continue app flow for permission granted in settings!
+        requestPermission();
+      }
+    }
+
+    this.setState({appState: nextAppState});
+  };
+
   render() {
-    const { isBlocked, onExitClick, onSettingsClick, onRequestClick } = this.props;
+    const { isBlocked, onExitClick, onSettingsClick, requestPermission } = this.props;
 
     return (
       <View
@@ -16,7 +47,7 @@ export default class GrantPermissionActions extends React.Component {
         { 
           isBlocked ?
           <FullWidthButton title="Settings..." onPress={onSettingsClick} /> :
-          <FullWidthButton title="Ask Permissions..." onPress={onRequestClick} />        
+          <FullWidthButton title="Ask Permissions..." onPress={requestPermission} />        
         }
       </View>
     );
