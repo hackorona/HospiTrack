1ba148ae92bf19887d9ba245cf6f688a24e30e50
Shachar Buda <shacharbuda27@gmail.com>
2020-03-25 14:43:59 +0200

    pass from container

--- client/App/Containers/NoPermissionsScreen/NoPermissionsScreen.js
+++ client/App/Containers/NoPermissionsScreen/NoPermissionsScreen.js
@@ -1,25 +1,50 @@
 import React from 'react'
 import { View } from 'react-native'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import { Helpers } from 'App/Theme'
 import GrantPermissionBody from '../../Components/GrantPermissionBody'
 import GrantPermissionActions from '../../Components/GrantPermissionActions'
+import { permissionsService } from '../../Services/PermissionsService';
+import PermissionsActions from '../../Stores/Permissions/Actions'
 
 class NoPermissionsScreen extends React.Component {
   render() {
+    const { isBlocked, requestPermissions } = this.props;
+
     return (
       <View
         style={[
           Helpers.fillColMain
         ]}
       >
-        <GrantPermissionBody />
-        <GrantPermissionActions />
+        <GrantPermissionBody isBlocked={isBlocked} />
+        <GrantPermissionActions
+          onExitClick={permissionsService.exitApp}
+          onSettingsClick={permissionsService.openSettings}
+          onRequestClick={requestPermissions}
+          isBlocked={isBlocked} />
       </View>
     )
   }
 }
 
 NoPermissionsScreen.propTypes = {
+  isGranted: PropTypes.bool.isRequired,
+  isBlocked: PropTypes.bool.isRequired,
+  requestPermissions: PropTypes.func.isRequired
 }
 
-export default NoPermissionsScreen;
+const mapStateToProps = (state) => ({
+  isGranted: state.permissions.granted,
+  isBlocked: state.permissions.blocked,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  requestPermissions: () => dispatch(PermissionsActions.permissionsRequest())
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NoPermissionsScreen)
