b4648b9a6d48acd218c5da18d2b0be7f863e5aa1
Shachar Buda <shacharbuda27@gmail.com>
2020-03-25 11:11:46 +0200

    No more example

--- client/App/Stores/index.js
+++ client/App/Stores/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux'
 import configureStore from './CreateStore'
 import rootSaga from 'App/Sagas'
-import { reducer as ExampleReducer } from './Example/Reducers'
 import { reducer as WifiReducer } from './Wifi/Reducers'
 
 export default () => {
@@ -10,7 +9,6 @@ export default () => {
      * Register your reducers here.
      * @see https://redux.js.org/api-reference/combinereducers
      */
-    example: ExampleReducer,
     wifi: WifiReducer
   })
 
