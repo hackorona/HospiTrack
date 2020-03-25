94f2d146555713e8f06774d884562e45f4cb071b
Shachar Buda <shacharbuda27@gmail.com>
2020-03-25 14:48:35 +0200

    switch to class component

--- client/App/Components/GrantPermissionActions.js
+++ client/App/Components/GrantPermissionActions.js
@@ -3,20 +3,22 @@ import { View } from 'react-native'
 import { Helpers } from '../Theme';
 import FullWidthButton from './FullWidthButton';
 
-export default function GrantPermissionActions(props) {
-  const { isBlocked, onExitClick, onSettingsClick, onRequestClick } = props;
+export default class GrantPermissionActions extends React.Component {
+  render() {
+    const { isBlocked, onExitClick, onSettingsClick, onRequestClick } = this.props;
 
-  return (
-    <View
-      style={[Helpers.row]}
-    >
-      {/* This might look oppsite in hebrew devices.. */}
-      <FullWidthButton title="Exit" onPress={onExitClick} />
-      { 
-        isBlocked ?
-        <FullWidthButton title="Settings..." onPress={onSettingsClick} /> :
-        <FullWidthButton title="Ask Permissions..." onPress={onRequestClick} />        
-      }
-    </View>
-  );
+    return (
+      <View
+        style={[Helpers.row]}
+      >
+        {/* This might look oppsite in hebrew devices.. */}
+        <FullWidthButton title="Exit" onPress={onExitClick} />
+        { 
+          isBlocked ?
+          <FullWidthButton title="Settings..." onPress={onSettingsClick} /> :
+          <FullWidthButton title="Ask Permissions..." onPress={onRequestClick} />        
+        }
+      </View>
+    );
+  }
 }
