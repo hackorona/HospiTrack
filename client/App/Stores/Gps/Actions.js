3adad28ed4dc891500d6c9e2940b3e9d0c5b19c5
Shachar Buda <shacharbuda27@gmail.com>
2020-03-26 12:55:51 +0200

    comment updates

--- client/App/Stores/Gps/Actions.js
+++ client/App/Stores/Gps/Actions.js
@@ -1,11 +1,11 @@
 import { createActions } from 'reduxsauce'
 
 const { Types, Creators } = createActions({
-  // Fetch user informations
+  // Fetch gps information
   fetchGpsLocation: null,
   // The operation has started and is loading
   fetchGpsLocationLoading: null,
-  // User informations were successfully fetched
+  // Gps informations were successfully fetched
   fetchGpsLocationSuccess: ['gpsLocation'],
   // An error occurred
   fetchGpsLocationFailure: ['errorMessage'],
