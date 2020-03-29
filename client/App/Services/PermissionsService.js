4c7fc588bae4e4da8d2fd72879e3f7e4c024a6e1
Shachar Buda <shacharbuda27@gmail.com>
2020-03-29 23:23:45 +0300

    request permission for imei

--- client/App/Services/PermissionsService.js
+++ client/App/Services/PermissionsService.js
@@ -3,19 +3,25 @@ import RNExitApp from 'react-native-exit-app';
 import OpenAppSettings from 'react-native-app-settings';
 
 const requestPermissions = async () => {
-  const grantedResult = await PermissionsAndroid.request(
+  const grantedResult = await PermissionsAndroid.requestMultiple([
     PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-    {
-      title: 'Location permission is required for WiFi connections',
-      message:
-        'This app needs location permission as this is required  ' +
-        'to scan for wifi networks.',
-      buttonNegative: 'DENY',
-      buttonPositive: 'ALLOW',
-    },
-  );
+    PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE
+  ]);
+    // TODO: return to single request of location once getting rid of imei
+    // const grantedResult = await PermissionsAndroid.request(
+      // PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      // {
+      //   title: 'Location permission is required for WiFi connections',
+      //   message:
+      //     'This app needs location permission as this is required  ' +
+      //     'to scan for wifi networks.',
+      //   buttonNegative: 'DENY',
+      //   buttonPositive: 'ALLOW',
+      // },
+    // );
 
-  return grantedResult;
+  // return grantedResult;
+  return grantedResult['android.permission.ACCESS_FINE_LOCATION'];
 };
 
 const askPermissions = async () => { 
