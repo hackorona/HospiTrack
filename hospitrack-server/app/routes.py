3f8c3a88e5a0a4d2777036417667fa95a0b76040
ronberkowitz <44921883+ronberkowitz@users.noreply.github.com>
2020-03-29 14:15:15 +0100

    added data insertion from payload

--- hospitrack-server/app/routes.py
+++ hospitrack-server/app/routes.py
@@ -1,44 +1,38 @@
 from app import app
 from app.controllers.router_scans import RouterScansController
 from app.controllers.demo_router_scans import DemoRouterScansController
-import time
 import json
+from flask import request, jsonify
+
 
 @app.route('/api/test')
 def test():
-    return 'success'
+    return jsonify(success=True)
 
-@app.route('/api/insert-router-scan')
+
+@app.route('/api/insert-router-scan', methods=['POST'])
 def insert_router_scan():
     """
     inserts a new router scan from a device into the database.
-    Should get the data from the request's payload from the client.
-    :return: success message
+    gets the data from the request's payload from the client.
+    :return: http response
     """
 
-    # TODO: get data from client's request payload
-
-
-    mock_data = {'imei': '123456789',
-                 'timestamp': int(time.time()),
-                 'longitude': 34.53,
-                 'latitude': 54.435,
-                 'altitude': 1.42,
-                 'accuracy': 9.33,
-                 'rssi_by_bssid': '{"krw:giwj:org": -70, "irgjw": -30, "rgj": 443}'
-                 }
-
-    new_router_scan = RouterScansController(imei=mock_data['imei'],
-                                            timestamp=mock_data['timestamp'],
-                                            longitude=mock_data['longitude'],
-                                            latitude=mock_data['latitude'],
-                                            altitude=mock_data['altitude'],
-                                            accuracy=mock_data['accuracy'],
-                                            rssi_by_bssid=mock_data['rssi_by_bssid']
+    data = request.form
+    new_router_scan = RouterScansController(imei=data['imei'],
+                                            timestamp=data['timestamp'],
+                                            longitude=data['longitude'],
+                                            latitude=data['latitude'],
+                                            altitude=data['altitude'],
+                                            accuracy=data['accuracy'],
+                                            rssi_by_bssid=data['rssi_by_bssid']
                                             )
 
     new_router_scan.insert()
-    return "successfuly inserted data"
+
+    resp = jsonify(success=True)
+    resp.status_code = 200
+    return resp
 
 
 @app.route('/api/insert-demo-data')
