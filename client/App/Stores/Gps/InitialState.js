8d0f2d14370ebb75dc1ba7149db921d5ca071c55
Shachar Buda <shacharbuda27@gmail.com>
2020-03-29 20:10:01 +0300

    undefined inital state

--- client/App/Stores/Gps/InitialState.js
+++ client/App/Stores/Gps/InitialState.js
@@ -2,7 +2,7 @@
  * The initial values for the redux state.
  */
 export const INITIAL_STATE = {
-  gpsLocation: {},
+  gpsLocation: undefined,
   gpsLocationIsLoading: false,
   gpsLocationErrorMessage: null,
   sampleSent: false
--- client/App/Stores/Wifi/InitialState.js
+++ client/App/Stores/Wifi/InitialState.js
@@ -2,7 +2,7 @@
  * The initial values for the redux state.
  */
 export const INITIAL_STATE = {
-  wifiList: [],
+  wifiList: undefined,
   wifiListIsLoading: false,
   wifiListErrorMessage: null,
   sampleSent: false
