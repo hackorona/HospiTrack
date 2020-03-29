81639c61576416cd2e44cfe255512ece26af8ccc
Shachar Buda <shacharbuda27@gmail.com>
2020-03-29 19:38:35 +0300

    no loops on wifi + gps sagas

--- client/App/Sagas/WifiSaga.js
+++ client/App/Sagas/WifiSaga.js
@@ -1,27 +1,15 @@
-import { put, call, delay } from 'redux-saga/effects'
+import { put, call } from 'redux-saga/effects'
 import WifiActions from '../Stores/Wifi/Actions'
 import { wifiService } from '../Services/WifiService'
-import { NEXT_SAMPLE_DELAY as DELAY } from '../Consts';
-
-// Temp, as repeat is till exiting hospital and is NOT a const.
-const REPEAT = 10;
 
 export function* fetchWifiList() {
-  let counter = REPEAT;
-  while (counter--) { 
-    // Dispatch a redux action using `put()`
-    yield put(WifiActions.fetchWifiListLoading())
+  yield put(WifiActions.fetchWifiListLoading())
 
+  try {
     // Fetch wifi informations from the wifiService
     const wifiArr = yield call(wifiService.fetchWifiList)
-    if (wifiArr) {
-      yield put(WifiActions.fetchWifiListSuccess(wifiArr))
-    } else {
-      yield put(
-        WifiActions.fetchWifiListFailure('There was an error while fetching Wifi list.')
-      );
-    }
-
-    yield delay(DELAY);
+    yield put(WifiActions.fetchWifiListSuccess(wifiArr))
+  } catch(e) {
+    yield put(WifiActions.fetchWifiListFailure(e));
   }
 }
