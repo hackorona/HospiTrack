d2ee968b43d876ff6b10f7786cb740af39fadec0
Ido Nov <idonov8@gmail.com>
2020-03-31 22:15:55 +0300

    add room id to redux sample actions

--- client/App/Stores/Samples/Actions.js
+++ client/App/Stores/Samples/Actions.js
@@ -3,6 +3,8 @@ import { createActions } from 'reduxsauce'
 const { Types, Creators } = createActions({
   startSample: null,
   sampleSent: null,
+  setRoomId: null,
+  clearRoomId: ['clear'],
 })
 
 export const SamplesTypes = Types
