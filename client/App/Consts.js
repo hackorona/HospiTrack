6c512228a2971830fde261647e65bd2c873f2e14
Shachar Buda <shacharbuda27@gmail.com>
2020-04-02 17:30:04 +0300

    use const url

--- client/App/Consts.js
+++ client/App/Consts.js
@@ -4,3 +4,8 @@ export const NEXT_SAMPLE_DELAY = ALWAYS_SAME_DELAY || 30000; // 4 scans in 2min 
 export const ANDROID10_SAMPLE_DELAY = ALWAYS_SAME_DELAY || 5000; // Android 10 gives option for more scans in dev settings
 export const ANDROID10_API_LEVEL = 29;
 export const GPS_TIMEOUT = NEXT_SAMPLE_DELAY;
+const SERVER_PREFIX_URL = 'https://hospitrack-app-api.azurewebsites.net/api';
+export const SERVER_URLS = {
+  IS_ALIVE: SERVER_PREFIX_URL + '/test',
+  POST_SAMPLE: SERVER_PREFIX_URL + '/insert-router-scan',
+}
--- client/App/Services/ApiService.js
+++ client/App/Services/ApiService.js
@@ -1,9 +1,9 @@
 import Axios from 'axios';
+import { SERVER_URLS } from '../Consts';
 
 // For now it always fails
 const writeSampleToServer = async (data) => Axios.post(
-  // TODO: pass to consts
-  'https://hospitrack-app-api.azurewebsites.net/api/insert-router-scan',
+  SERVER_URLS.POST_SAMPLE,
   data,
   // TODO: decide timeout
   {timeout: 1000}
