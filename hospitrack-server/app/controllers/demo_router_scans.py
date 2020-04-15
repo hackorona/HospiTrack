3e98f2621b4e00e0aa81f43ae0b2cc631d2bc0a5
ronberkowitz <44921883+ronberkowitz@users.noreply.github.com>
2020-04-15 23:49:15 +0100

    removed demo data

--- hospitrack-server/app/controllers/demo_router_scans.py
+++
@@ -1,9 +0,0 @@
-from app.models.demo_router_scans import DemoRouterScans
-from app.controllers.base_controller import BaseController
-from app.controllers import session
-
-class DemoRouterScansController(DemoRouterScans, BaseController):
-
-    @staticmethod
-    def select_all_rows():
-        return session.query(DemoRouterScans).all()
--- hospitrack-server/app/routes.py
+++ hospitrack-server/app/routes.py
@@ -2,8 +2,6 @@ from app import app
 from app.utils.data_utils import set_default_values
 from app.controllers.router_scans import RouterScansController
 from app.controllers.labeled_router_scans import LabeledRouterScansController
-from app.controllers.demo_router_scans import DemoRouterScansController
-import json
 from flask import request, jsonify
 
 
@@ -62,38 +60,3 @@ def insert_router_scan():
     resp.status_code = 200
     return resp
 
-
-@app.route('/api/insert-demo-data')
-def insert_demo_data():
-    """
-    this was only made for inserting the demo data to the db.
-    after the real data is being inserted this can be deleted.
-    :return: success message
-    """
-    new_li = []
-    columns = ['id', 'latitude', 'longitude', 'altitude', 'accuracy']
-    with open(r'app/data/json_log_example.json') as f:
-        dct = json.load(f)
-        for key in dct:
-            for el in dct[key]:
-                new_row = {}
-                router_jsons = {}
-                new_row['timestamp'] = key
-                for field in el:
-                    if field in columns:
-                        new_row[field] = el[field]
-                    else:
-                        router_jsons[field] = el[field]
-                new_row['rssi_by_bssid'] = router_jsons
-                new_li.append(DemoRouterScansController(
-                    id=new_row['id'],
-                    timestamp=new_row['timestamp'],
-                    latitude=new_row['latitude'],
-                    longitude=new_row['longitude'],
-                    altitude=new_row['altitude'],
-                    accuracy=new_row['accuracy'],
-                    rssi_by_bssid=new_row['rssi_by_bssid']
-                ))
-
-    DemoRouterScansController.insert_many(new_li)
-    return f'inserted {len(new_li)} records successfully'
