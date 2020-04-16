db98ebfc44e3907c2cf9955566164ef7a9072597
Shachar Buda <shacharbuda27@gmail.com>
2020-04-16 18:31:36 +0300

    fix import

--- client/App/Containers/LoggingSamples/LoggingSamplesScreen.js
+++ client/App/Containers/LoggingSamples/LoggingSamplesScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View, Image } from 'react-native'
+import { Text, View, Image, Button } from 'react-native'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import SamplesActions from '../../Stores/Samples/Actions'
