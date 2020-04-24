51008dc26a482def2775d859d3b45fd500efee70
Shachar Buda <shacharbuda27@gmail.com>
2020-04-24 15:32:32 +0300

    resolve

--- client/App/Services/AndroidForegroundService.js
+++ client/App/Services/AndroidForegroundService.js
@@ -1,41 +1,50 @@
 /* eslint-disable prettier/prettier */
 import ForegroundService from 'react-native-foreground-service';
 
+const TASK_NAME = 'dataSample';
+
+let resolveFunc;
+
 const initForegroundService = async () => {
-    let resolveFunc;
-    const foregroundTask = async (data) => {
+  const foregroundTask = async (data) => {
+    console.log('foregrondTask initialized');
     return new Promise((resolve) => {
-        resolveFunc = () => {
-        console.warn('resolveFunc()');
-        resolve();
-        };
+      // Pass to "global" var
+      resolveFunc = resolve;
     });
-    }
+  }
 
-    ForegroundService.registerForegroundTask("dataSample", foregroundTask);
+  ForegroundService.registerForegroundTask(TASK_NAME, foregroundTask);
 
-    const notificationConfig = {
-        id: 1,
-        title: 'Service',
-        message: `HospiTrack is keeping you safe`,
-        visibility: 'public',
-        importance: 'low',
-        number: 55
-    };
+  // TODO: play with configs
+  const notificationConfig = {
+    id: 1,
+    title: 'Service',
+    message: `HospiTrack is keeping you safe`,
+    visibility: 'public',
+    importance: 'low',
+    number: 55
+  };
 
-    await ForegroundService.startService(notificationConfig);
-    await ForegroundService.runTask({
-    taskName: 'dataSample',
+  await ForegroundService.startService(notificationConfig);
+  await ForegroundService.runTask({
+    taskName: TASK_NAME,
     delay: 0
-    }); 
+  });
 }
 
 const stopForegroundService = async () => {
-    await ForegroundService.stopServiceAll();
-    console.log("STOPPED");
+  if (resolveFunc) {
+    console.log('resolving task');
+    resolveFunc();
+  } else {
+    throw new Error(`Can't stop foreground service if didn't start`);
+  }
+  await ForegroundService.stopServiceAll();
+  console.log("STOPPED");
 }
 
 export const AndroidForegroundService = {
-    initForegroundService,
-    stopForegroundService
+  initForegroundService,
+  stopForegroundService
 }
